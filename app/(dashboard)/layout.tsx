a0db6b3a0c26f508f2f4dd182b2f711e2c09e280
benhazoom <87472603+benhazoom@users.noreply.github.com>
2025-02-27 10:50:38 +0200

    User button moved to upper bar
    using slots prop of DashboardLayout mui toolpad component

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,14 +1,25 @@
+'use client'
+
 import * as React from 'react';
 import { DashboardLayout } from '@toolpad/core/DashboardLayout';
 import { PageContainer } from '@toolpad/core/PageContainer';
 import { UserButton } from '@clerk/nextjs';
 
+function CustomUserButton() {
+  return (
+
+    <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+      <UserButton showName />
+    </div>
+  );
+}
+
 export default function Layout(props: { children: React.ReactNode }) {
   return (
-    <DashboardLayout>
-      <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-        <UserButton showName />
-      </div>
+    <DashboardLayout
+      slots={{
+        toolbarAccount: CustomUserButton
+      }}>
       <PageContainer>{props.children}</PageContainer>
     </DashboardLayout>
   );
