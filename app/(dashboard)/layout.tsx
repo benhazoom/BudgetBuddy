ce5372de4c04255666a970531ec0ab9b68dff156
benhazoom <benhazoom12@gmail.com>
2025-03-26 12:59:56 +0200

    deployment fixes

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,32 +1,39 @@
-'use client'
+"use client";
 
-import * as React from 'react';
-import { DashboardLayout } from '@toolpad/core/DashboardLayout';
-import { PageContainer } from '@toolpad/core/PageContainer';
-import { UserButton } from '@clerk/nextjs';
-import { Chip, Stack, Typography, Tooltip } from '@mui/material';
-import { CloudCircle as CloudCircleIcon, CheckCircle as CheckCircleIcon } from '@mui/icons-material';
+import * as React from "react";
+import { DashboardLayout } from "@toolpad/core/DashboardLayout";
+import { PageContainer } from "@toolpad/core/PageContainer";
+import { UserButton } from "@clerk/nextjs";
+import { Chip, Stack, Typography, Tooltip } from "@mui/material";
+import {
+  CloudCircle as CloudCircleIcon,
+  CheckCircle as CheckCircleIcon,
+} from "@mui/icons-material";
 
 function CustomUserButton() {
   return (
-
-    <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+    <div style={{ display: "flex", justifyContent: "flex-end" }}>
       <UserButton />
     </div>
-  )
+  );
 }
 function CustomAppTitle() {
   return (
-
     <Stack direction="row" alignItems="center" spacing={2}>
-      <img src="/budgetbuddy_logo.png" alt="Budget Buddy Logo" style={{ height: 40 }} />
-      <Typography variant="h6" style={{ fontWeight: 'bold' }}>Budget Buddy</Typography>
+      <img
+        src="/budgetbuddy_logo.png"
+        alt="Budget Buddy Logo"
+        style={{ height: 40 }}
+      />
+      <Typography variant="h6" style={{ fontWeight: "bold" }}>
+        Budget Buddy
+      </Typography>
       <Chip size="small" label="Alpha" color="info" />
       {/* <Tooltip title="Connected to production">
         <CheckCircleIcon color="success" fontSize="small" />
       </Tooltip> */}
     </Stack>
-  )
+  );
 }
 
 export default function Layout(props: { children: React.ReactNode }) {
@@ -34,9 +41,10 @@ export default function Layout(props: { children: React.ReactNode }) {
     <DashboardLayout
       slots={{
         appTitle: CustomAppTitle,
-        toolbarAccount: CustomUserButton
-      }}>
+        toolbarAccount: CustomUserButton,
+      }}
+    >
       <PageContainer>{props.children}</PageContainer>
     </DashboardLayout>
   );
-}  
+}
